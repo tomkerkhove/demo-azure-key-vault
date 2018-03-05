0e1366116d59c167c1da2910b51015639375cb51
Tom Kerkhove <kerkhove.tom@gmail.com>
2018-03-05 12:34:37 -0800

    Add scenario 1 & 2

--- src/TomKerkhove.Samples.KeyVault.API/Controllers/Scenario1Controller.cs
+++ src/TomKerkhove.Samples.KeyVault.API/Controllers/Scenario1Controller.cs
@@ -1,22 +1,56 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.KeyVault;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using Swashbuckle.AspNetCore.SwaggerGen;
 
 namespace TomKerkhove.Samples.KeyVault.API.Controllers
 {
-    [Route("api/v1/secrets/basic-auth/", Name="Secrets (Basic Authentication)")]
+    [Route("api/v1/secrets/basic-auth/", Name = "Secrets (Basic Authentication)")]
     public class Scenario1Controller : Controller
     {
+        // You should never do this, but it's a demo so why bother!
+        private readonly string adApplicationId = "666ef5f5-017d-4f01-b105-54fea4d9618f";
+        private readonly string adApplicationSecret = "oKQTcEHlIZ7WKAiXqKt0DSC+i1HMOOueQnoHtXORpPs=";
+        private readonly string vaultUri = "https://secure-applications.vault.azure.net/";
+        
         [HttpGet("{secretName}")]
         [SwaggerOperation("Get Secret (Basic Authentication)")]
-        public string Get(string secretName)
+        public async Task<string> Get(string secretName)
         {
-            return "value";
+            var keyVaultClient = GetKeyVaultClient();
+            var secret = await keyVaultClient.GetSecretAsync(vaultUri, secretName);
+
+            return secret.Value;
         }
 
         [HttpPut("{secretName}")]
         [SwaggerOperation("Set Secret (Basic Authentication)")]
-        public void Put(string secretName, [FromBody] string secretValue)
+        public async Task Put(string secretName, [FromBody] string secretValue)
         {
+            var keyVaultClient = GetKeyVaultClient();
+            await keyVaultClient.SetSecretAsync(vaultUri, secretName, secretValue);
+        }
+
+        private async Task<string> AuthenticationCallback(string authority, string resource, string scope)
+        {
+            var authContext = new AuthenticationContext(authority);
+            var clientCredential = new ClientCredential(adApplicationId, adApplicationSecret);
+            var token = await authContext.AcquireTokenAsync(resource, clientCredential).ConfigureAwait(continueOnCapturedContext: false);
+
+            if (token == null)
+            {
+                throw new InvalidOperationException("Failed to obtain a token");
+            }
+
+            return token.AccessToken;
+        }
+
+        private KeyVaultClient GetKeyVaultClient()
+        {
+            return new KeyVaultClient(AuthenticationCallback);
         }
     }
 }
---
+++ src/TomKerkhove.Samples.KeyVault.API/Controllers/Scenario2Controller.cs
@@ -0,0 +1,39 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace TomKerkhove.Samples.KeyVault.API.Controllers
+{
+    [Route("api/v1/secrets/managed-service-identity/", Name = "Secrets (Managed Service Identity)")]
+    public class Scenario2Controller : Controller
+    {
+        private readonly string vaultUri = "<Configure-Me>";
+
+        [HttpGet("{secretName}")]
+        [SwaggerOperation("Get Secret (Basic Authentication)")]
+        public async Task<string> Get(string secretName)
+        {
+            var keyVaultClient = GetKeyVaultClient();
+            var secret = await keyVaultClient.GetSecretAsync(vaultUri, secretName);
+
+            return secret.Value;
+        }
+
+        [HttpPut("{secretName}")]
+        [SwaggerOperation("Set Secret (Basic Authentication)")]
+        public async Task Put(string secretName, [FromBody] string secretValue)
+        {
+            var keyVaultClient = GetKeyVaultClient();
+            await keyVaultClient.SetSecretAsync(vaultUri, secretName, secretValue);
+        }
+
+        private static KeyVaultClient GetKeyVaultClient()
+        {
+            var azureServiceTokenProvider = new AzureServiceTokenProvider();
+            var keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+            return keyVaultClient;
+        }
+    }
+}
